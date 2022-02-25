9e2b9f9b8bd89b1dd9b28d2d22f5b605babeaa90
Prashant Aarya <97784813+prashantaarya@users.noreply.github.com>
2022-02-25 15:37:22 +0530

    Add files via upload

---
+++ qnabotcovid-19-bot-src (1)/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnabotcovid-19-bot-9dc2-Web-Deploy.pubxml -p:Password=rsGelgAoGxyGdQx7bFCuBrowHz36orGwme8fNuj3PvtGsfXAD605416HuDer
+
